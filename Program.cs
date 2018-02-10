d21f7bb2165115da844efbb75f256397acb4dd5d
Ronny Chan <ronny6993@gmail.com>
2018-02-09 22:46:31 -0500

    Summary
    Changed: My Changed
    Flaw: My Flaw
    Fix: My Fix

---
+++ Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.IO;
+
+namespace git_commit_msg_notes
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+            string commitMessage = File.ReadAllText(".git/COMMIT_EDITMSG");
+            Console.WriteLine(commitMessage);
+            Environment.Exit(1);
+        }
+    }
+}
